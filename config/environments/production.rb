ac56dc409411d24c938917ffeb087d53457c6ca7
Alex Pavluck <apavluck@gmail.com>
2014-06-17 13:33:20 -0400

    added config needed to get devise to work

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,7 @@ Pinteresting::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  
+  # Required for Huroku
+  config.action_mailer.default_url_options = { host: 'http://apavluck.herokuapp.com/' }
 end
