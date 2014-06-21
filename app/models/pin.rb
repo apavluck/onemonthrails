68a547b21079490ebfac0781f77a6abce02f37e1
Alex Pavluck <apavluck@gmail.com>
2014-06-21 11:04:02 -0400

    added paperclip and image magic, modify database, show, upload, allow

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,3 +1,5 @@
 class Pin < ActiveRecord::Base
   belongs_to :user
+  
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "http://elec-tra-mate.com/store/sc_images/products/1_missing_thumbnail_image.jpg"
 end
