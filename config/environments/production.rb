54737be1fc6b08d841c5232cd1055bfc1737ecc4
Alex Pavluck <apavluck@gmail.com>
2014-06-21 16:38:01 -0400

    added amazon s3 support for paperclip env values

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,14 @@ Pinteresting::Application.configure do
   
   # Required for Huroku
   config.action_mailer.default_url_options = { host: 'http://apavluck.herokuapp.com/' }
+  
+  #sets paperclip to upload images to aws s3
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['AWS_BUCKET'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
 end
